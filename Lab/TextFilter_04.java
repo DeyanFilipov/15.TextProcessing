683fbf8fc3ad35b2c8a00700f81e7f1b9774fb52
DeyanFilipov <didkofilipov.sv@gmail.com>
2023-03-09 19:58:22 +0200

    Commit

---
+++ Lab/TextFilter_04.java
@@ -0,0 +1,25 @@
+package TextProcessing.Lab;
+
+import java.util.Scanner;
+
+public class TextFilter_04 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] banWordsArr = scanner.nextLine().split(", ");
+        String text = scanner.nextLine();
+
+        for (String banWord : banWordsArr) {
+            String asterisk = repeatString(banWord.length());
+            text = text.replace(banWord, asterisk);
+        }
+        System.out.println(text);
+    }
+    public static String repeatString (int count) {
+        String result = "";
+        for (int i = 0; i < count; i++) {
+            result += "*";
+        }
+        return result;
+    }
+}
