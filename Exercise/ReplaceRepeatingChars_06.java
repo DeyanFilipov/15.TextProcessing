e5c27775cb647798a95e7075364429301b3ef69f
DeyanFilipov <didkofilipov.sv@gmail.com>
2023-03-11 10:15:01 +0200

    Commit

---
+++ Exercise/ReplaceRepeatingChars_06.java
@@ -0,0 +1,27 @@
+package TextProcessing.Exercise;
+
+import java.util.Scanner;
+
+public class ReplaceRepeatingChars_06 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < input.length(); i++) {
+            if (i == input.length() - 1 && input.charAt(i) == input.charAt(i - 1)) {
+                sb.append(input.charAt(i));
+                break;
+            } else if (i == input.length() - 1 && input.charAt(i) != input.charAt(i - 1)) {
+                sb.append(input.charAt(i));
+                break;
+            } else {
+                if (input.charAt(i) != input.charAt(i + 1)) {
+                    sb.append(input.charAt(i));
+                }
+            }
+        }
+        System.out.println(sb);
+    }
+}
