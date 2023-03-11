25696e9dd0134eacd7a61c8342d985d9c5b0b1f3
DeyanFilipov <didkofilipov.sv@gmail.com>
2023-03-11 22:29:40 +0200

    Commit

---
+++ MoreExercises/AsciiSumator_02.java
@@ -0,0 +1,22 @@
+package TextProcessing.MoreExercises;
+
+import java.util.Scanner;
+
+public class AsciiSumator_02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        char one = scanner.nextLine().charAt(0);
+        char two = scanner.nextLine().charAt(0);
+        String input = scanner.nextLine();
+        int sum = 0;
+
+        for (int i = 0; i < input.length(); i++) {
+            if (one > input.charAt(i) && two < input.charAt(i) ||
+                    two > input.charAt(i) && one < input.charAt(i)) {
+                sum += input.charAt(i);
+            }
+        }
+        System.out.println(sum);
+    }
+}
